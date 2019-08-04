00e247a9f56344196ce744bb6bbf512e3656f3a3
Rafael Cadaval <rafaelcadaval@gmail.com>
2019-08-04 20:25:58 -0300

    Add EventsTableViewCell and initial layout

---
+++ challenge-ss/challenge-ss/Cells/EventsTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  EventsTableViewCell.swift
+//  challenge-ss
+//
+//  Created by Rafael Cadaval on 04/08/19.
+//  Copyright © 2019 Rafael Cadaval. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EventsTableViewCell: UITableViewCell {
+    @IBOutlet weak var eventImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+}
