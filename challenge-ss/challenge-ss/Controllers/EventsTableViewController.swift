e573050bccd10a0cb29b2cd1456b0d1173beb0bc
Rafael Cadaval <rafaelcadaval@gmail.com>
2019-08-05 08:53:32 -0300

    Update `EventsTableViewController`
    `EventsListViewModel` is added to the controller, as well as the main
    methods for the tableView usage.

--- challenge-ss/challenge-ss/Controllers/EventsTableViewController.swift
+++ challenge-ss/challenge-ss/Controllers/EventsTableViewController.swift
@@ -8,8 +8,11 @@
 
 import Foundation
 import UIKit
+import Kingfisher
 
 class EventsTableViewController: UITableViewController {
+    var eventsListViewModel: EventsListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,19 +21,52 @@ class EventsTableViewController: UITableViewController {
         fetchEvents()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // This ensures that the constraints of height and anchors can be enforced correctly
+        tableView.estimatedRowHeight = 170
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+    
     private func fetchEvents() {
         guard let url = URL(string: "http://5b840ba5db24a100142dcd8c.mockapi.io/api/events") else {
             fatalError("Incorrect URL")
         }
         
         let resource = Resource<[Event]>(url: url)
-        WebService.load(resource: resource) { result in
+        WebService.load(resource: resource) { [weak self] result in
             switch result {
             case .success(let events):
-                print(events)
+                self?.eventsListViewModel = EventsListViewModel(events)
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()                    
+                }
             case .failure(let error):
                 print(error)
             }
         }
+    }
+}
+
+extension EventsTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return eventsListViewModel == nil ? 0 : eventsListViewModel.eventsViewModel.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let viewModel = eventsListViewModel.eventViewModel(at: indexPath.row)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "EventsTableViewCell", for: indexPath) as? EventsTableViewCell else { fatalError("Could not dequeue EventsTableViewCell") }
+        let processor = DownsamplingImageProcessor(size: CGSize(width: 150, height: 120))
+        cell.eventImage.kf.indicatorType = .activity
+        cell.eventImage.kf.setImage(with: viewModel.imageURL, placeholder: UIImage(named: "placeholderEventImage"), options: [.processor(processor), .scaleFactor(UIScreen.main.scale), .transition(.fade(0.5))])
+        cell.titleLabel.text = viewModel.title
+        cell.priceLabel.text = viewModel.price
+        
+        return cell
     }
 }
